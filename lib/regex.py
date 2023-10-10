7d174d9b41de9fee094be0d11d420d011181c921
RegisLewis <regisaslewis@gmail.com>
2023-10-09 20:03:29 -0700

    regex validation lab

--- lib/regex.py
+++ lib/regex.py
@@ -9,5 +9,5 @@ name_regex = re.compile(name)
 phone_number = r"\({0,1}[0-9]{3}\){0,1}[ \-]{0,1}[0-9]{3}[\-]{0,1}[0-9]{4}"
 phone_regex = re.compile(phone_number)
 
-email_address = r"^[^\d][.A-z0-9]+@[a-z]+.[a-z]{3}"
+email_address = r"^[^\d][.A-z0-9]+@[a-z]+\.[a-z]{3,}"
 email_regex = re.compile(email_address)
